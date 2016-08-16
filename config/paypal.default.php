5cb619acafec8b054fcc3007359d1c3d7eeac754
HumanG33k <HumanG33k@users.noreply.github.com>
2016-08-16 15:58:04 +0200

    Add a paypal subscribe form using IPN. Send the request work but not the callback. Commit to test on the server.

---
+++ config/paypal.default.php
@@ -0,0 +1,48 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+return [
+    
+    /*
+     * Website price informations
+     *  months => prices
+     *  
+     * 
+     */
+    
+    
+    'Site' => [
+            'prices'=> [
+                         6  => 42,
+                         12 => 72,
+                         24 => 50,
+            ]
+    ],
+    /*
+     * Paypal api/nvp informations
+     * 
+     */  
+    'Paypal'=> [
+                'email' => '',
+                'USER' => '',
+                'PWD' => '',
+                'SIGNATURE'=> '',
+                'sandbox' => ''
+],
+    
+];
+
+
+
+
+
+
+
